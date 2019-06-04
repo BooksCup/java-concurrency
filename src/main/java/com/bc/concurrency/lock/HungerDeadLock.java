243ed9e89d1b5845d478a3f77c08add1f68fe45f
BooksCup <812809680@qq.com>
2019-06-04 09:26:44 +0800

    线程饥饿问题添加注释

--- src/main/java/com/bc/concurrency/lock/HungerDeadLock.java
+++ src/main/java/com/bc/concurrency/lock/HungerDeadLock.java
@@ -11,6 +11,16 @@ public class HungerDeadLock {
 
     ExecutorService exec = Executors.newSingleThreadExecutor();
 
+    /**
+     * 模拟页面加载的例子
+     *
+     * 产生死锁分析：
+     * RenderPageTask任务中有2个子任务分别是“加载页眉”和“加载页脚”。
+     * 当提交RenderPageTask任务时，实际上是向线程池中添加了3个任务，
+     * 但是由于线程池是单一线程池，同时只会执行一个任务，2个子任务就会在阻塞在线程池中。
+     * 而RenderPageTask任务由于得不到返回，也会一直堵塞，不会释放线程资源让子线程执行。这样就导致了线程饥饿死锁。
+     *
+     */
     class RenderPageTask implements Callable<String> {
         @Override
         public String call() throws Exception {
