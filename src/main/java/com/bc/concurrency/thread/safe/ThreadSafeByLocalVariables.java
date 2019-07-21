5fe3e92a9193158bd98517d2019eb26933fe1f33
BooksCup <812809680@qq.com>
2019-07-21 10:13:30 +0800

    使用局部变量规避线程安全性问题

---
+++ src/main/java/com/bc/concurrency/thread/safe/ThreadSafeByLocalVariables.java
@@ -0,0 +1,31 @@
+package com.bc.concurrency.thread.safe;
+
+public class ThreadSafeByLocalVariables extends Thread {
+
+    @Override
+    public void run() {
+        double[] scores = new double[]{1.0, 2.0, 3.0};
+        double avgScore = avgScore(scores);
+        System.out.println(Thread.currentThread().getName() + ", avgScore: " + avgScore);
+    }
+
+    public static void main(String[] args) {
+        ThreadSafeByLocalVariables threadSafeByLocalVariables = new ThreadSafeByLocalVariables();
+        Thread t1 = new Thread(threadSafeByLocalVariables, "t1");
+        Thread t2 = new Thread(threadSafeByLocalVariables, "t2");
+        Thread t3 = new Thread(threadSafeByLocalVariables, "t3");
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+
+    private double avgScore(double[] scores) {
+        double sum = 0;
+        for (double score : scores) {
+            sum += score;
+        }
+        int count = scores.length;
+        double avg = sum / count;
+        return avg;
+    }
+}
